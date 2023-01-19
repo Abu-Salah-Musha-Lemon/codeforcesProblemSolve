ebd5a2e5c02bbad1a54e3afb852264c353529f94
Abu-Salah-Musha-Lemon <114002239+Abu-Salah-Musha-Lemon@users.noreply.github.com>
2023-01-19 17:51:02 +0600

    Create DominoPiling.cpp

---
+++ DominoPiling.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int m, n;
+    std::cin >> m >> n;
+    std::cout << (m * n) / 2 << std::endl;
+    return 0;
+}
+// secound solution
+#include <iostream>
+using namespace std;
+int main() {
+    int x,y,domino;
+    cout<< "enter the size of box"<<endl;
+    cin>> x>>y;
+    domino = (x*y)/2;
+    cout<< "the ammount of domino put in :"<< domino<< endl;
+    return 0;
+}
