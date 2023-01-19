b153ac6561f72b3f8876b4fc21fe7d78d660b31b
Abu-Salah-Musha-Lemon <114002239+Abu-Salah-Musha-Lemon@users.noreply.github.com>
2023-01-19 19:10:58 +0600

    Update 1772A - A+B?.cpp

--- 1772A - A+B?.cpp
+++ 1772A - A+B?.cpp
@@ -14,3 +14,20 @@ int a, b;
     }
     return 0;
 }
+
+// c -programe 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+
+    int t;
+
+    scanf("%d",&t);
+    while(t--)
+    {
+        char a[5];
+        scanf("%s",&a);
+        printf("%d\n",a[0]+a[2]-96);
+    }
+}
