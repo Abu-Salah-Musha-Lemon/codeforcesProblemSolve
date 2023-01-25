2a2bbec5c292f1f62ae6ca6f69751137d61f8be9
Abu-Salah-Musha-Lemon <114002239+Abu-Salah-Musha-Lemon@users.noreply.github.com>
2023-01-25 22:25:51 +0600

    Create 339A - Helpful Maths.cpp

---
+++ 339A - Helpful Maths.cpp
@@ -0,0 +1,26 @@
+/*
+input : 3 + 2 + 1
+output: 1 + 2 + 3
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin>> s;
+    for(int i =0 ; i<s.size(); i+=2)
+    {
+        for(int j=0; j<s.size()-1; j+=2)
+        {
+            if(s[j]>s[j+2])
+            {
+                swap(s[j],s[j+2]);
+            }
+        }
+    }
+
+    cout<< s <<endl;
+}
